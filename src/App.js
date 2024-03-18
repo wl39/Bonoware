9a5a504cb5b06e7d72080715d88ecd3acf796f52
wl39 <iwg6852@gmail.com>
2024-03-18 19:40:35 +0000

    BrowserRouter changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import LeftHeaderLinks from "components/Header/LeftHeaderLinks.jsx";
 import {
   MuiThemeProvider,
   createMuiTheme,
-  withStyles
+  withStyles,
 } from "@material-ui/core/styles";
 import Footer from "components/Footer/Footer.jsx";
 import { hot } from "react-hot-loader";
@@ -21,121 +21,121 @@ const THEME = createMuiTheme({
     fontSize: 14,
     fontWeightLight: 300,
     fontWeightRegular: 400,
-    fontWeightMedium: 500
+    fontWeightMedium: 500,
   },
   body: {
     fontFamily: "'Noto Sans KR', sans-serif",
-    fontWeight: 300
-  }
+    fontWeight: 300,
+  },
 });
 
-const Loading = () => <div></div>;
+const Loading = () => <div />;
 
 const MainPageLoader = Loadable({
   loader: () => import("views/MainPage/MainPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const ProductDetailPageLoader = Loadable({
   loader: () => import("views/ProdcutDetailPage/ProductDetailPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const NFCDetailPageLoader = Loadable({
   loader: () => import("views/NFCDetailPage/NFCDetailPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const ProductLoader = Loadable({
   loader: () => import("views/ProductPage/ProductPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const SellLoader = Loadable({
   loader: () => import("views/SellPage/SellPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const DetailSellLoader = Loadable({
   loader: () => import("views/SellPage/DetailSell.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const AboutPageLoader = Loadable({
   loader: () => import("components/About/About.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const AdminPageLoader = Loadable({
   loader: () => import("views/AdminPage/AdminPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const PrivacyPageLoader = Loadable({
   loader: () => import("views/PrivacyPage/PrivacyPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const TermsPageLoader = Loadable({
   loader: () => import("views/TermsPage/TermsPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const TestPage2Loader = Loadable({
   loader: () => import("views/TestPage2/TestPage2.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const TestPageLoader = Loadable({
   loader: () => import("views/TestPage/TestPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const RegisterPageLoader = Loadable({
   loader: () => import("views/Forms/Wizard.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const ProfilePageLoader = Loadable({
   loader: () => import("views/ProfilePage/ProfileContent.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const ProfileContentoader = Loadable({
   loader: () => import("views/ProfilePage/ProfileContent.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const PaymentsCompleteLoader = Loadable({
   loader: () => import("views/Payments/Complete.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const EmailCompleteLoader = Loadable({
   loader: () => import("views/EmailVerification/Complete.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const ShoeNewReleasesPageLoader = Loadable({
   loader: () => import("views/NewReleases/NewReleasesPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const ShoeNewsPageLoader = Loadable({
   loader: () => import("views/ShoeNews/ShoeNewsPage.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 const ShoeNewsLoader = Loadable({
   loader: () => import("views/ShoeNews/ShoeNews.jsx"),
-  loading: Loading
+  loading: Loading,
 });
 
 class App extends Component {
   state = {
     mobileOpen: false,
     miniActive: false,
-    searchBar: false
+    searchBar: false,
   };
   // async componentWillMount() {
   //   this.props.getMainImagesHandler()
@@ -146,11 +146,11 @@ class App extends Component {
   componentWillReceiveProps() {
     if (window.location.pathname === "/") {
       this.setState({
-        searchBar: false
+        searchBar: false,
       });
     } else {
       this.setState({
-        searchBar: true
+        searchBar: true,
       });
     }
   }
@@ -159,18 +159,18 @@ class App extends Component {
   componentWillMount() {
     if (window.location.pathname === "/") {
       this.setState({
-        searchBar: false
+        searchBar: false,
       });
     } else {
       this.setState({
-        searchBar: true
+        searchBar: true,
       });
     }
   }
 
   handleDrawerToggle = () => {
     this.setState({
-      mobileOpen: !this.state.mobileOpen
+      mobileOpen: !this.state.mobileOpen,
     });
   };
 
@@ -222,7 +222,7 @@ class App extends Component {
             leftLinks={null}
             changeColorOnScroll={{
               height: 100,
-              color: "white"
+              color: "white",
             }}
             {...rest}
           />
